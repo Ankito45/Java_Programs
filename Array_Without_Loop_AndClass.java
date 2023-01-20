bedfae1a89d414df918e27fa7a8da6c45a07cce6
anki012 <108117695+anki012@users.noreply.github.com>
2023-01-20 20:00:52 +0530

    Add files via upload

---
+++ Array_Without_Loop_AndClass.java
@@ -0,0 +1,14 @@
+package corejava;
+
+import java.util.Arrays;
+
+public class Array_Without_Loop_AndClass {
+
+	public static void main(String[] args) {
+		int[] Arr = {55,77,88,99,44,22,33};
+//		int x = 5;
+//		String sc = Integer.toString(x);
+//		System.out.println(x);
+		System.out.println("Array :" + Arrays.toString(Arr));
+	}
+}
